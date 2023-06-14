007f440359d3e11d0e9d162b93322a8b49ef4827
Archit Gupta <gupta.archit01@gmail.com>
2023-06-14 08:00:16 +0530

    Added

--- C++/vector.cpp
+++ C++/vector.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 
-
-void display(vector<int> &v){
+template<class T>
+void display(vector<T> &v){
+    cout<<"displaying";
     for(int i=0; i<v.size();i++){
         cout<<v[i]<<" ";
 
@@ -13,15 +14,25 @@ void display(vector<int> &v){
     cout<<endl;
 }
 int main(){
+    vector<int> v(6,3); //6 element vector of 3s
     vector<int> vec1;
+    vector<char> vec2(4);  // 4 element character vector
+    vector<char> vec3(vec2);  // 4 element character vector from vect2
 
 
-    int element;
+    int element, size=5;
+
+    display(v);
 
     for(int i=0;i<4;i++){
         cout<<"Enter an element to add to this vector";
         cin>>element;
         vec1.push_back(element);
+    }
+    for(int i=0;i<size;i++){
+        cout<<"Enter an element to add to this vector";
+        cin>>element;
+        vec2.push_back(element);
     }
 
     display(vec1);
@@ -30,11 +41,11 @@ int main(){
     cout<<"Enter an element to add to this vector";
 
     vector<int> :: iterator iter = vec1.begin();
-    vec1.insert(iter,566);  //
+    vec1.insert(vec1.begin(),566);  //
     vec1.insert(iter+1,566);
     vec1.insert(iter+1,500,566);
     display(vec1);
     
-    return 0;
+    // return 0;
 
 }
