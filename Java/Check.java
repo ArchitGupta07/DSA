1ba1b5849aa89e19e84fb1ae205e7785d1897858
Archit Gupta <gupta.archit01@gmail.com>
2023-07-22 13:35:52 +0530

    Updated

---
+++ Java/Check.java
@@ -0,0 +1,24 @@
+package Java;
+import java.util.Scanner;
+
+public class Check {
+
+
+ public static void main(String[] args) {
+    System.out.println("tAKING INPUT FROM THE USER");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a");
+    int a = sc.nextInt();
+    System.out.println("Enter b");
+    int b = sc.nextInt();
+    
+    System.out.println(a);
+    System.out.println(b);
+
+ }
+
+
+
+    
+}
--- Java/main.java
+++ Java/main.java
@@ -4,4 +4,23 @@ package Java;
 
 public class Main{
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        char ch = 'A';
+        long l = 566666666666l;
+
+
+        float f1 = 5.6f;
+        double d1 = 5.6;
+
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ch);
+        System.out.println(l);
+        
+    }
+
+
+
 }
