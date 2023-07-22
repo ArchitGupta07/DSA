a731e27580641be369ce3c3f768b817ecc3c2c51
Archit Gupta <gupta.archit01@gmail.com>
2023-07-22 16:48:15 +0530

    Update Check.java

--- Java/Check.java
+++ Java/Check.java
@@ -8,13 +8,18 @@ public class Check {
     System.out.println("tAKING INPUT FROM THE USER");
 
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a");
-    int a = sc.nextInt();
-    System.out.println("Enter b");
-    int b = sc.nextInt();
+    // System.out.println("Enter a");
+    // int a = sc.nextInt();
+    // System.out.println("Enter b");
+    // int b = sc.nextInt();
     
-    System.out.println(a);
-    System.out.println(b);
+    // System.out.println(a);
+    // System.out.println(b);
+
+
+    String str = sc.next(); // take just one word as input
+    String str1 = sc.nextLine(); // take whole line as input
+
 
  }
 
