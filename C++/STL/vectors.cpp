5c9a6a848da17433f91f7f3656b433d39167e705
Archit Gupta <gupta.archit01@gmail.com>
2023-07-25 17:25:47 +0530

    Update vectors.cpp

--- C++/STL/vectors.cpp
+++ C++/STL/vectors.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 #include<math.h>
 #include<string.h>
+#include <vector> // Include the vector header
 
 using namespace std;
 
 
 int main(){
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+
 
 
 
