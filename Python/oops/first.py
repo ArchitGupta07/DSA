0ee1b01fd4ad1195eb863847d8736d3d4b4ed237
Archit Gupta <gupta.archit01@gmail.com>
2023-08-07 20:50:12 +0530

    Update first.py

---
+++ Python/oops/first.py
@@ -0,0 +1,14 @@
+class Person:
+
+    def __init__(self,name="aastha",age=18) -> None:
+        self.name = name
+        self.age = age
+    
+   
+
+a = Person()
+print(a.name)
+b=Person('Archit',22)
+
+print(b.name)
+
